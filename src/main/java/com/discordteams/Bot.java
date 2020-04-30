84b19996a2a31662b98b72505f761fba18d5acac
plantstoen <plantstoen@gmail.com>
2020-04-30 21:45:15 +0900

    add: token reader

--- src/main/java/com/discordteams/Bot.java
+++ src/main/java/com/discordteams/Bot.java
@@ -9,16 +9,23 @@ import net.dv8tion.jda.api.JDA;
 
 import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 public class Bot {
 
     public static JDA jda;
 
     public static void main(String[] args) throws Exception {
+        File file = new File("/Users/plantstoen/Dev/DiscordTeams/src/main/resources/token.txt");
+        FileReader filereader = new FileReader(file);
+        BufferedReader bufReader = new BufferedReader(filereader);
+
         JDABuilder jb = new JDABuilder(AccountType.BOT);
         jb.setAutoReconnect(true);
         jb.setStatus(OnlineStatus.DO_NOT_DISTURB);
-        jb.setToken("NzA1MzQ1NzUxOTI4MDEyODI5.XqrAMg.HdRzlD5NGYuslFdVDgXpNeX3iug");
+        jb.setToken(bufReader.readLine());
 
         try {
             jda = jb.build();
